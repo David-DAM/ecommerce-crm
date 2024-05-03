0a21c168cac39c9b1c0a0d09656044bdbdcc19ab
David <david11jv@gmail.com>
2024-05-03 13:37:05 +0200

    Initial commit

---
+++ src/main/java/com/david/ecommercecrm/EcommerceCrmApplication.java
@@ -0,0 +1,13 @@
+package com.david.ecommercecrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/david/ecommercecrm/EcommerceCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.ecommercecrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
